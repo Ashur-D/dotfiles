a928018e73562c03af5631778da6017612578d79
Ashur-D <ashur25batto@gmail.com>
2025-10-25 12:39:14 -0400

    updated

--- scripts/neededpackages.sh
+++ scripts/neededpackages.sh
@@ -7,6 +7,8 @@
 
 # List of packages to install
 packages=(
+  kitty-git
+  #hypr
   hyprland-git
   hyprpaper-git
   #hyprland-qtutils-git
@@ -16,15 +18,25 @@ packages=(
   hypridle-git
   #hyprpolkitagent-git
   xdg-desktop-portal-hyprland-git
+  #################################################
   qt6-wayland
   qt5-wayland
   ttf-cascadia-mono-nerd
   wireplumber-git
   pipewire-git
-  neovim-git
-  mako-git
-  waybar-git
-  walker-bin
+  neovim-git 
+  mako-git # notifcation deamon
+  waybar-git # topbar
+  walker-bin # application launcher/dmenu > planning to change to hyprlauncher
+  bluetui # bluetooth tui
+  impala # wifi tui
+  wiremix-git # audio tui
+  gpu-screen-recorder # or wl-screenrec
+  slurp-git
+  wl-clipboard-git
+  wl-clip-persist-git
+  grim-git
+  satty-git
 )
 
 # Check for yay
