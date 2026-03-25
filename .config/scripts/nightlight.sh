58302e88ca69f154b66e76274a8b45cf5860dd73
Ashur-D <ashurdawod@proton.me>
2026-03-25 15:23:23 -0400

    updated bashrc, waybar, swayosd, and nightlight.sh

--- .config/scripts/nightlight.sh
+++ .config/scripts/nightlight.sh
@@ -8,5 +8,6 @@ else
     # If it is NOT running, start it at 4000K (Nightlight Mode)
     # The '&' pushes it to the background so the script can finish
     hyprsunset -t 4000 &
+    hyprctl hyprsunset gamma 80
     notify-send "  Nightlight Mode" "Screen temperature set to 4000K."
 fi
