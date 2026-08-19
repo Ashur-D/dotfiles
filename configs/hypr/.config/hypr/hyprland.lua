6f78ebbab6e03d1e0b40cf6309ded23548d17e5d
Ashur-D <ashurdawod@proton.me>
2026-08-19 18:32:21 -0400

    Revert Hyprland Lua migration back to commit 7075cba

--- configs/hypr/.config/hypr/hyprland.lua
+++
@@ -1,8 +0,0 @@
--- Main Hyprland Configuration Entrypoint
-require("hypr.monitor")
-require("hypr.envs")
-require("hypr.looknfeel")
-require("hypr.input")
-require("hypr.bindings")
-require("hypr.autostart")
-require("hypr.windows")
