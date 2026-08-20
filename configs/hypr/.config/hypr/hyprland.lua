8ab472c52a1982f1b2543d3a6c2d7351cbdb5212
Ashur-D <ashurdawod@proton.me>
2026-08-19 20:51:56 -0400

    updated

--- configs/hypr/.config/hypr/hyprland.lua
+++ configs/hypr/.config/hypr/hyprland.lua
@@ -1,12 +1,13 @@
 -- ==========================
--- Hyprland source files
+-- Hyprland Main Config
 -- ==========================
 
-require("hyprbindings")
-require("hyprenvs")
-require("hyprinput")
-require("hyprlooknfeel")
-require("hyprmonitor")
-require("hyprwindows")
-require("hyprautostart")
+local config_dir = os.getenv("HOME") .. "/.config/hypr/"
 
+dofile(config_dir .. "hyprautostart.lua")
+dofile(config_dir .. "hyprenvs.lua")
+dofile(config_dir .. "hyprmonitor.lua")
+dofile(config_dir .. "hyprinput.lua")
+dofile(config_dir .. "hyprlooknfeel.lua")
+dofile(config_dir .. "hyprbindings.lua")
+dofile(config_dir .. "hyprwindows.lua")
