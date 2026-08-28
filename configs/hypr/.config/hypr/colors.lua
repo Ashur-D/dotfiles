041068407a7b3cae57d6d40cefe72b35ab30c170
Ashur <119370859+Ashur-D@users.noreply.github.com>
2026-08-28 18:37:34 -0400

    Add color configuration for Hyprland

--- configs/hypr/.config/hypr/colors.lua
+++ configs/hypr/.config/hypr/colors.lua
@@ -1,1 +1,106 @@
 # will be over-written
+
+return {
+    image = "/home/ash/dotfiles/media/wallpapers/wallpaper91.jpg",
+
+    background = "0xff0c1512",
+
+    error = "0xffffb4ab",
+
+    error_container = "0xff93000a",
+
+    inverse_on_surface = "0xff29322f",
+
+    inverse_primary = "0xff934a2d",
+
+    inverse_surface = "0xffdbe5df",
+
+    on_background = "0xffdbe5df",
+
+    on_error = "0xff690005",
+
+    on_error_container = "0xffffdad6",
+
+    on_primary = "0xff581e04",
+
+    on_primary_container = "0xffffdbce",
+
+    on_primary_fixed = "0xff370d00",
+
+    on_primary_fixed_variant = "0xff753318",
+
+    on_secondary = "0xff00363a",
+
+    on_secondary_container = "0xffbcebef",
+
+    on_secondary_fixed = "0xff002022",
+
+    on_secondary_fixed_variant = "0xff1e4d51",
+
+    on_surface = "0xffdbe5df",
+
+    on_surface_variant = "0xffb9cbc2",
+
+    on_tertiary = "0xff00382e",
+
+    on_tertiary_container = "0xffabf0dd",
+
+    on_tertiary_fixed = "0xff00201a",
+
+    on_tertiary_fixed_variant = "0xff005144",
+
+    outline = "0xff83958d",
+
+    outline_variant = "0xff3a4a44",
+
+    primary = "0xffffb59a",
+
+    primary_container = "0xff753318",
+
+    primary_fixed = "0xffffdbce",
+
+    primary_fixed_dim = "0xffffb59a",
+
+    scrim = "0xff000000",
+
+    secondary = "0xffa0cfd3",
+
+    secondary_container = "0xff1e4d51",
+
+    secondary_fixed = "0xffbcebef",
+
+    secondary_fixed_dim = "0xffa0cfd3",
+
+    shadow = "0xff000000",
+
+    source_color = "0xff1b2a20",
+
+    surface = "0xff0c1512",
+
+    surface_bright = "0xff323b37",
+
+    surface_container = "0xff19211e",
+
+    surface_container_high = "0xff232c28",
+
+    surface_container_highest = "0xff2e3733",
+
+    surface_container_low = "0xff151d1a",
+
+    surface_container_lowest = "0xff07100d",
+
+    surface_dim = "0xff0c1512",
+
+    surface_tint = "0xffffb59a",
+
+    surface_variant = "0xff3a4a44",
+
+    tertiary = "0xff8fd4c2",
+
+    tertiary_container = "0xff005144",
+
+    tertiary_fixed = "0xffabf0dd",
+
+    tertiary_fixed_dim = "0xff8fd4c2",
+
+}
